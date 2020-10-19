f50d91ab18d768582be8df47a7f0c1ec7d478554
parmendra05 <parmendrakumar718@gmail.com>
2020-10-19 16:26:13 +0530

    ..

---
+++ 02 Spring-boot-H2 Configration/src/main/java/com/pack/Model/Product.java
@@ -0,0 +1,71 @@
+package com.pack.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int price;
+	private String size;
+	private String color;
+	
+	public Product(){}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", size=" + size + ", color=" + color
+				+ "]";
+	}
+	
+	
+	
+}
---
+++ 02 Spring-boot-H2 Configration/src/test/java/com/example/Spring/boot/Tymeleaf/SpringBootTymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.boot.Tymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
